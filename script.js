b4350cb39f93b26ae99f68df2bffc4a2b98f3f6d
Aaron Williams <polarizedcuriosity@gmail.com>
2023-07-10 17:05:23 -0700

    I added the buttons to the num-pad div.

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(input) {
+  input + input;
+  return result;
+  console.log(result);
+}
+
+function subtract(input) {
+  return input - input;
+}
+
+function multiply(input) {
+  return input * input;
+}
+
+function divide(input) {
+  return input / input;
+}
